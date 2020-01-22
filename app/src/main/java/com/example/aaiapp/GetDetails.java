3d9d436c8469196c71edaede06a98d366a04c1a3
jaswanth9959 <jayreddy731@gmail.com>
2020-01-22 14:03:43 +0530

    added details page and class

--- app/src/main/java/com/example/aaiapp/GetDetails.java
+++ app/src/main/java/com/example/aaiapp/GetDetails.java
@@ -1,19 +1,83 @@
 package com.example.aaiapp;
 
+
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Bundle;
 
-public class GetDetails extends AsyncTask<String, Void, Void> {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class GetDetails extends AsyncTask<String, Void, String> {
     @SuppressLint("StaticFieldLeak")
+
+
     private Context context;
 
-    GetDetails(Context ctx) {
-        context = ctx;
+    GetDetails(Context ctx)
+    {
+        context=ctx;
     }
 
+
     @Override
-    protected Void doInBackground(String... params) {
+    protected String doInBackground(String... params) {
+
+        String type=params[1];
+        String details_url = "http://192.168.0.6/details.php";
+        if(type.equals("info")){
+            try {
+                String id=params[0];
+
+                URL url=new URL(details_url);
+                HttpURLConnection httpsURLConnection = (HttpURLConnection) url.openConnection();
+                httpsURLConnection.setRequestMethod("POST");
+                httpsURLConnection.setDoOutput(true);
+                httpsURLConnection.setDoInput(true);
+                OutputStream outputStream = httpsURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8));
+                String post_data = URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(id, "UTF-8") ;                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpsURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.ISO_8859_1));
+                String result="";
+                String line;
+                while((line=bufferedReader.readLine())!=null)
+                {
+                    System.out.println(line);
+                    result = result.concat(line);
+
+                }
+                System.out.println();
+                bufferedReader.close();
+                inputStream.close();
+                httpsURLConnection.disconnect();
+                System.out.println(result);
+                return result;
+            } catch (MalformedURLException e) {
+                System.out.println("catch1");
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.out.println("catch2");
+                e.printStackTrace();
+            }
+
+        }
+
         return null;
     }
 
@@ -23,8 +87,12 @@ public class GetDetails extends AsyncTask<String, Void, Void> {
     }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
+    protected void onPostExecute(String result) {
+        Intent intent = new Intent(context, InfoDetails.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("message", result);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
     }
 
     @Override
---
+++ app/src/main/java/com/example/aaiapp/InfoDetails.java
@@ -0,0 +1,47 @@
+package com.example.aaiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.StringTokenizer;
+
+public class InfoDetails extends AppCompatActivity {
+    String message;
+    String[] details=new String[4];
+    private TextView name,mobile,roll,id;
+
+    InfoDetails(String msg)
+    {
+        message=msg;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_details);
+        name=findViewById(R.id.machinename);
+        mobile=findViewById(R.id.machinemobile);
+        roll=findViewById(R.id.machineroll);
+        id=findViewById(R.id.machineid);
+
+        Bundle bundle = getIntent().getExtras();
+        message = bundle.getString("message");
+        execute();
+        name.setText(details[0]);
+        mobile.setText(details[1]);
+        roll.setText(details[2]);
+        id.setText(details[3]);
+    }
+    public void execute()
+    {
+        StringTokenizer st=new StringTokenizer(message,":");
+        int i=0;
+        while(st.hasMoreTokens())
+        {
+            details[i]=st.nextToken();
+            i+=1;
+        }
+    }
+}
